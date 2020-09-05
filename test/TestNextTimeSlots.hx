084807a7b9e9f25c241546c38a7d2926e8013064
Andy Li <andy@onthewings.net>
2020-09-05 13:51:07 +0800

    update timeslot for test

--- test/TestNextTimeSlots.hx
+++ test/TestNextTimeSlots.hx
@@ -6,8 +6,8 @@ class TestNextTimeSlots extends utest.Test {
         var nextSlots = DragonJapaneseCuisine.nextTimeSlots(Date.fromString("2020-08-15 23:49:00"));
         Assert.same(
             [
-                "2020-08-16 12:00:00",
-                "2020-08-16 13:00:00",
+                "2020-08-16 12:30:00",
+                "2020-08-16 13:30:00",
                 "2020-08-16 19:00:00",
                 "2020-08-16 20:00:00",
             ],
@@ -20,8 +20,8 @@ class TestNextTimeSlots extends utest.Test {
         var nextSlots = DragonJapaneseCuisine.nextTimeSlots(Date.fromString("2020-08-17 09:59:59"));
         Assert.same(
             [
-                "2020-08-17 12:00:00",
-                "2020-08-17 13:00:00",
+                "2020-08-17 12:30:00",
+                "2020-08-17 13:30:00",
                 "2020-08-17 19:00:00",
                 "2020-08-17 20:00:00",
             ],
@@ -34,8 +34,8 @@ class TestNextTimeSlots extends utest.Test {
             [
                 "2020-08-17 19:00:00",
                 "2020-08-17 20:00:00",
-                "2020-08-18 12:00:00",
-                "2020-08-18 13:00:00",
+                "2020-08-18 12:30:00",
+                "2020-08-18 13:30:00",
             ],
             nextSlots.map(slot -> DateTools.format(slot.start, "%Y-%m-%d %H:%M:%S"))
         );
@@ -48,8 +48,8 @@ class TestNextTimeSlots extends utest.Test {
             [
                 "2020-08-17 19:00:00",
                 "2020-08-17 20:00:00",
-                "2020-08-18 12:00:00",
-                "2020-08-18 13:00:00",
+                "2020-08-18 12:30:00",
+                "2020-08-18 13:30:00",
             ],
             nextSlots.map(slot -> DateTools.format(slot.start, "%Y-%m-%d %H:%M:%S"))
         );
@@ -58,8 +58,8 @@ class TestNextTimeSlots extends utest.Test {
         var nextSlots = DragonJapaneseCuisine.nextTimeSlots(Date.fromString("2020-08-17 17:00:01"));
         Assert.same(
             [
-                "2020-08-18 12:00:00",
-                "2020-08-18 13:00:00",
+                "2020-08-18 12:30:00",
+                "2020-08-18 13:30:00",
                 "2020-08-18 19:00:00",
                 "2020-08-18 20:00:00",
             ],
@@ -73,8 +73,8 @@ class TestNextTimeSlots extends utest.Test {
             [
                 false, // 2020-08-17 19:00:00
                 false, // 2020-08-17 20:00:00
-                true,  // 2020-08-18 12:00:00
-                true,  // 2020-08-18 13:00:00
+                true,  // 2020-08-18 12:30:00
+                true,  // 2020-08-18 13:30:00
             ],
             nextSlots.map(slot -> slot.isOff)
         );
