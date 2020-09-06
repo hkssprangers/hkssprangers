f51b4145381597cd1faa94de5a14b5dbed5d3e4f
Andy Li <andy@onthewings.net>
2020-09-07 00:49:07 +0800

    gen moment extern

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,4 +7,4 @@ lix download
 mkdir -p node_modules
 sudo chmod a+rwx node_modules
 yarn
-npx dts2hx telegraf @types/js-cookie --noLibWrap --output lib/dts2hx
+npx dts2hx telegraf @types/js-cookie moment --noLibWrap --output lib/dts2hx
