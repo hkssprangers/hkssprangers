aa5316738bc05cddb71dcca63bddc5ee261897f4
Andy Li <andy@onthewings.net>
2020-10-19 18:53:39 +0800

    add drop statement when dumping db structure

--- dev/initdb/001_structure.sql
+++ dev/initdb/001_structure.sql
@@ -1,4 +1,4 @@
--- MariaDB dump 10.17  Distrib 10.5.5-MariaDB, for debian-linux-gnu (x86_64)
+-- MariaDB dump 10.17  Distrib 10.5.6-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: mysql    Database: hkssprangers
 -- ------------------------------------------------------
@@ -18,6 +18,8 @@
 --
 -- Current Database: `hkssprangers`
 --
+
+/*!40000 DROP DATABASE IF EXISTS `hkssprangers`*/;
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `hkssprangers` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
@@ -135,4 +137,4 @@ CREATE TABLE `order` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-18 14:41:57
+-- Dump completed on 2020-10-19 10:46:34
--- dumpDb.sh
+++ dumpDb.sh
@@ -1,4 +1,4 @@
 set -ev
-mysqldump --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --no-data --databases hkssprangers | sed 's/ AUTO_INCREMENT=[0-9]*//g' > dev/initdb/001_structure.sql
+mysqldump --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --no-data --databases hkssprangers --add-drop-database | sed 's/ AUTO_INCREMENT=[0-9]*//g' > dev/initdb/001_structure.sql
 mysqldump --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --no-create-db --no-create-info --skip-extended-insert hkssprangers courier > dev/initdb/002_data_courier.sql
-mysqldump --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --no-create-db --no-create-info --skip-extended-insert --ignore-table=hkssprangers.courier hkssprangers > dev/initdb/003_data.sql
+mysqldump --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --no-create-db --no-create-info --skip-extended-insert hkssprangers --ignore-table=hkssprangers.courier > dev/initdb/003_data.sql
