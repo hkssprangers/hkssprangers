ad575bf13d569587ece86a2053c060eb0b8a04eb
Andy Li <andy@onthewings.net>
2020-10-27 18:14:56 +0800

    remove instant.page
    this is not quite a static site anyway

--- src/hkssprangers/server/View.hx
+++ src/hkssprangers/server/View.hx
@@ -57,7 +57,6 @@ class View extends ReactComponent {
     function footJs() return jsx('
         <Fragment>
             <script src=${R("/trackExceptions.js")}></script>
-            <script src="//instant.page/3.0.0" type="module" defer integrity="sha384-OeDn4XE77tdHo8pGtE1apMPmAipjoxUQ++eeJa6EtJCfHlvijigWiJpD7VDPWXV1"></script>
         </Fragment>
     ');
 
