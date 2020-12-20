751a34cb5480b1b55f2258bf8c499cd7a126f896
Andy Li <andy@onthewings.net>
2020-12-21 02:06:39 +0800

    嘉美中心's delivery fee

--- src/hkssprangers/DeliveryFee.hx
+++ src/hkssprangers/DeliveryFee.hx
@@ -688,6 +688,17 @@ class DeliveryFee {
                 case MGYCluster: 35;
             }
         },
+        {
+            place: "嘉美中心",
+            match: address -> address.contains("嘉美中心"),
+            deliveryFee: cluster -> switch cluster {
+                case DragonCentreCluster: 25;
+                case YearsCluster: 25;
+                case BiuKeeCluster: 25;
+                case NeighborCluster: 25;
+                case MGYCluster: 25;
+            }
+        },
     ];
 
     static public function decideDeliveryFee(shop:Shop, address:String):Null<Float> {
