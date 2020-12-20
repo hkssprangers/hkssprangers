305ac6af825e0103648efa0eff79be2dff86512f
Andy Li <andy@onthewings.net>
2020-12-20 10:18:08 +0800

    順景閣's delivery fee

--- src/hkssprangers/DeliveryFee.hx
+++ src/hkssprangers/DeliveryFee.hx
@@ -677,6 +677,17 @@ class DeliveryFee {
                 case MGYCluster: 25;
             }
         },
+        {
+            place: "順景閣",
+            match: address -> address.contains("順景閣"),
+            deliveryFee: cluster -> switch cluster {
+                case DragonCentreCluster: 25;
+                case YearsCluster: 25;
+                case BiuKeeCluster: 25;
+                case NeighborCluster: 25;
+                case MGYCluster: 35;
+            }
+        },
     ];
 
     static public function decideDeliveryFee(shop:Shop, address:String):Null<Float> {
