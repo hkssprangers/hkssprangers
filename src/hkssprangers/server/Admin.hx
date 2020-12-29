2674d4ddbe5cf1d797d426b07fd87abb3cac596b
Andy Li <andy@onthewings.net>
2020-12-29 16:53:21 +0800

    debug

--- src/hkssprangers/server/Admin.hx
+++ src/hkssprangers/server/Admin.hx
@@ -93,7 +93,9 @@ class Admin extends View {
     static final hr = "\n--------------------------------------------------------------------------------\n";
 
     static public function setTg(req:Request, reply:Reply):Promise<Null<Tg>> {
+        trace("setTg");
         var cookies = req.getCookies();
+        trace(cookies);
         var tg:Dynamic = if (cookies != null && cookies.tg != null) try {
             Json.parse(cookies.tg);
         } catch(err) {
@@ -157,6 +159,7 @@ class Admin extends View {
     }
 
     static public function ensurePermission(req:Request, reply:Reply):Promise<Bool> {
+        trace("ensurePermission");
         return setTg(req, reply)
             .then(_ -> setCourier(req, reply))
             .then(_ -> setToken(req, reply))
@@ -363,15 +366,18 @@ class Admin extends View {
     }
 
     static public function get(req:Request, reply:Reply):Promise<Dynamic> {
+        trace("get admin");
         return ensurePermission(req, reply)
             .then(ok -> {
                 if (!ok) {
                     var url = new node.url.URL(req.raw.url, "http://example.com");
                     return Promise.resolve(reply.redirect("/login?redirectTo=" + (url.pathname + url.search).urlEncode()));
                 }
+                trace("ok");
                 var user = reply.getCourier();
                 switch (req.accepts().type(["text/html", "application/json"])) {
                     case "text/html":
+                        trace("text/html");
                         var tgBotInfo = tgBot.telegram.getMe();
                         tgBotInfo.then(tgBotInfo ->
                             reply.sendView(Admin, {
@@ -382,6 +388,7 @@ class Admin extends View {
                             }))
                             .catchError(err -> reply.status(500).send(err));
                     case "application/json":
+                        trace("application/json");
                         var token = reply.getToken();
                         switch (token) {
                             case null:
@@ -430,7 +437,8 @@ class Admin extends View {
                                     });
                                 }
                             });
-                    case _:
+                    case type:
+                        trace(type);
                         reply.type("text");
                         Promise.resolve(reply.status(406).send("Can only return html or json"));
                 }
