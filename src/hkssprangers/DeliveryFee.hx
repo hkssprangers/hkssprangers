6acefd0e16969dbc544d5156214d56450501aa55
Andy Li <andy@onthewings.net>
2021-01-01 13:59:33 +0800

    友來大廈's delivery fee

--- src/hkssprangers/DeliveryFee.hx
+++ src/hkssprangers/DeliveryFee.hx
@@ -722,6 +722,17 @@ class DeliveryFee {
                 case MGYCluster: 25;
             }
         },
+        {
+            place: "友來大廈",
+            match: address -> address.contains("友來大廈"),
+            deliveryFee: cluster -> switch cluster {
+                case DragonCentreCluster: 25;
+                case YearsCluster: 25;
+                case BiuKeeCluster: 25;
+                case NeighborCluster: 35;
+                case MGYCluster: 25;
+            }
+        },
     ];
 
     static public function decideDeliveryFee(shop:Shop, address:String):Null<Float> {
