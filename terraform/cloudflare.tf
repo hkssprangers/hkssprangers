34353f430a72171909c39b71467856a92772d2b5
Andy Li <andy@onthewings.net>
2021-01-03 21:25:24 +0800

    import some dns records

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -8,3 +8,31 @@ resource "cloudflare_record" "aws-acm" {
   value   = trimsuffix(tolist(aws_acm_certificate.default.domain_validation_options).0.resource_record_value, ".")
   type    = tolist(aws_acm_certificate.default.domain_validation_options).0.resource_record_type
 }
+
+resource "cloudflare_record" "ssprangers" {
+  zone_id = cloudflare_zone.ssprangers.id
+  name    = "ssprangers.com"
+  value   = "production.ssprangers.com"
+  type    = "CNAME"
+}
+
+resource "cloudflare_record" "production" {
+  zone_id = cloudflare_zone.ssprangers.id
+  name    = "production"
+  value   = "d-h25rgwffz8.execute-api.ap-southeast-1.amazonaws.com"
+  type    = "CNAME"
+}
+
+resource "cloudflare_record" "master" {
+  zone_id = cloudflare_zone.ssprangers.id
+  name    = "master"
+  value   = "d-8njywa7so9.execute-api.ap-southeast-1.amazonaws.com"
+  type    = "CNAME"
+}
+
+resource "cloudflare_record" "dev" {
+  zone_id = cloudflare_zone.ssprangers.id
+  name    = "dev"
+  value   = "d-ip6lixd84a.execute-api.ap-southeast-1.amazonaws.com"
+  type    = "CNAME"
+}
