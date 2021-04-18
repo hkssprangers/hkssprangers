a3cecc6d8a7be7ce42a153d03a2ce6985e8e8bbb
Andy Li <andy@onthewings.net>
2021-04-18 23:28:12 +0800

    cache node and haxe libs in the devcontainer

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TAG="hkssprangers/hkssprangers_devcontainer_workspace:$(date +%Y%m%d%H%M%S)"
 
+cp -r "$DIR/../.haxerc" "$DIR"/../*.hxml "$DIR/../haxe_libraries" "$DIR/../package.json" "$DIR/../yarn.lock" "$DIR/workspace/"
 docker build --pull -t "$TAG" "$DIR"
 
-yq eval ".services.workspace.image = \"$TAG\"" "$DIR/.devcontainer/docker-compose.yml" -i
-yq eval ".jobs.build.container = \"$TAG\"" "$DIR/.github/workflows/ci.yml" -i
+yq eval ".services.workspace.image = \"$TAG\"" "$DIR/docker-compose.yml" -i
+yq eval ".jobs.build.container = \"$TAG\"" "$DIR/../.github/workflows/ci.yml" -i
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 set -ex
-
-lix download
-
-mkdir -p node_modules
-sudo chmod a+rwx node_modules
-yarn --network-concurrency 1 # https://github.com/yarnpkg/yarn/issues/6312
-npx dts2hx telegraf @types/js-cookie moment copy-to-clipboard fastify @types/accepts aws-serverless-fastify google-auth-library abort-controller @types/promise-retry ajv node-ical --noLibWrap --output lib/dts2hx
-
-pip3 install docx2txt
+yarn
+yarn tailwind
+yarn dts2hx
