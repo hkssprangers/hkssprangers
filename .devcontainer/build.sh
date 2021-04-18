4b6342006592a1b89c1f25ce47799c76b1400e59
Andy Li <andy@onthewings.net>
2021-04-18 17:21:15 +0800

    rebase the devcontainer

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -5,4 +5,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TAG="hkssprangers/hkssprangers_devcontainer_workspace:$(date +%Y%m%d%H%M%S)"
 
-docker build -t "$TAG" "$DIR"
+docker build --pull -t "$TAG" "$DIR"
+
+yq eval ".services.workspace.image = \"$TAG\"" "$DIR/.devcontainer/docker-compose.yml" -i
+yq eval ".jobs.build.container = \"$TAG\"" "$DIR/.github/workflows/ci.yml" -i
