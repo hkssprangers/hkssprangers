51c91fa8951390719593425c25c1fc077d28e8c1
Andy Li <andy@onthewings.net>
2021-07-24 16:26:42 +0800

    let service worker cache more

--- src/hkssprangers/serviceWorker/ServiceWorkerMain.hx
+++ src/hkssprangers/serviceWorker/ServiceWorkerMain.hx
@@ -4,13 +4,42 @@ import workbox_core.*;
 import workbox_cacheable_response.*;
 import workbox_strategies.*;
 import workbox_expiration.*;
+using StringTools;
 
 class ServiceWorkerMain {
 	static function main() {
+		WorkboxRouting.setDefaultHandler(new NetworkFirst());
 		WorkboxRouting.registerRoute((options:RouteMatchCallbackOptions) -> {
             StaticResource.parseUrl(options.url.pathname).hash != null;
         }, new CacheFirst({
 			cacheName: "static",
+			plugins: ([
+				new CacheableResponsePlugin({
+					statuses: [0, 200],
+				}),
+				new ExpirationPlugin({
+					maxEntries: 60,
+				}),
+			]:Array<Dynamic>),
+		}));
+		WorkboxRouting.registerRoute(function(options:RouteMatchCallbackOptions) {
+			if (switch options.request.destination {
+				case FONT | IMAGE | SCRIPT | STYLE:
+					var url = options.request.url;
+					url.startsWith("https://cdn.jsdelivr.net/") ||
+					url.startsWith("https://cdn.lordicon.com/") ||
+					url.startsWith("https://fonts.googleapis.com/") ||
+					url.startsWith("https://fonts.gstatic.com/");
+				case _:
+					false;
+			}) return true;
+
+			if (options.request.url.startsWith("https://cdn.lordicon.com/") && options.request.url.endsWith(".json"))
+				return true;
+
+			return false;
+		}, new CacheFirst({
+			cacheName: "external_static",
 			plugins: ([
 				new CacheableResponsePlugin({
 					statuses: [0, 200],
