f1f955ba8eb6043beb510bca461b4b4670f072b8
Andy Li <andy@onthewings.net>
2021-09-11 18:19:22 +0800

    add more MGY dishes

--- src/hkssprangers/info/menu/MGYMenu.hx
+++ src/hkssprangers/info/menu/MGYMenu.hx
@@ -67,6 +67,11 @@ class MGYMenu {
                     "高湯浸菠菜 $" + markup(38),
                     "咸酸菜炒芽葉 $" + markup(58),
                     "白灼羽衣甘藍 $" + markup(28),
+
+                    "七彩孜然排骨 $" + markup(58),
+                    "欖菜豆腐 $" + markup(58),
+                    "蟠龍如 $" + markup(98),
+                    "叉燒炒三絲 $" + markup(62),
                 ]
             },
         },
