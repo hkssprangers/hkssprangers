79ffb3942bdf38a640a8625e6acfa50f1e02a512
Andy Li <andy@onthewings.net>
2021-09-18 11:48:29 +0800

    景翠苑's delivery fee

--- src/hkssprangers/DeliveryFee.hx
+++ src/hkssprangers/DeliveryFee.hx
@@ -423,6 +423,19 @@ class DeliveryFee {
             }
         },
         {
+            place: "景翠苑",
+            match: address -> address.contains("景翠苑"),
+            deliveryFee: cluster -> switch cluster {
+                case DragonCentreCluster: 25;
+                case CLPCluster: 25;
+                case GoldenCluster: 25;
+                case SmilingPlazaCluster: 25;
+                case ParkCluster: 25;
+                case PakTinCluster: 25;
+                case TungChauStreetParkCluster: 35;
+            }
+        },
+        {
             place: "置輝閣",
             match: address -> address.contains("置輝閣"),
             deliveryFee: cluster -> switch cluster {
