de95bcb1d4eac628de46ad8574d57779c76a1967
Andy Li <andy@onthewings.net>
2021-10-09 18:09:36 +0800

    better wording

--- src/hkssprangers/info/Shop.hx
+++ src/hkssprangers/info/Shop.hx
@@ -488,7 +488,7 @@ enum abstract Shop(String) to String {
             case [ThaiYummy, _, _]:
                 return Unavailable('埗兵外賣暫停');
             case [_, "2021-10-09", _]:
-                return Unavailable('打風 休息一天');
+                return Unavailable('打風 埗兵休息一天');
             case [LaksaStore, "2021-10-10", _]:
                 return Unavailable('冇食材 休息一天');
             case _:
