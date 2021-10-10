e483697eb22180237e2681d90528ad59bca88d58
Andy Li <andy@onthewings.net>
2021-10-11 00:22:01 +0800

    add new chok powders

--- src/hkssprangers/info/menu/ZeppelinHotDogSKMMenu.hx
+++ src/hkssprangers/info/menu/ZeppelinHotDogSKMMenu.hx
@@ -45,8 +45,8 @@ class ZeppelinHotDogSKMMenu {
     static public final chokOptions = [
         "CHOK CHOK粉 芥末紫菜",
         "CHOK CHOK粉 地道椒鹽",
-        // "CHOK CHOK粉 惹味麻辣",
-        // "CHOK CHOK粉 香甜蕃茄",
+        "CHOK CHOK粉 惹味麻辣",
+        "CHOK CHOK粉 香甜蕃茄",
     ];
 
     static public final ZeppelinHotDogSKMSingle = {
