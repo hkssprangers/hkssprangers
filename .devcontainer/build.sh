7014fcabc48d109e5b66f189eafbda0053bd4c5b
Andy Li <andy@onthewings.net>
2021-11-17 15:51:02 +0800

    fix dts2hx

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 IMAGE="hkssprangers/hkssprangers_devcontainer_workspace"
 TAG="${IMAGE}:$(date +%Y%m%d%H%M%S)"
 
-cp -r "$DIR/../.haxerc" "$DIR"/../*.hxml "$DIR/../haxe_libraries" "$DIR/../package.json" "$DIR/../yarn.lock" "$DIR/workspace/"
+cp -r "$DIR/../.haxerc" "$DIR"/../*.hxml "$DIR/../haxe_libraries" "$DIR/../package.json" "$DIR/../yarn.lock" "$DIR/../gen-externs.sh" "$DIR/workspace/"
 docker build --pull -t "$TAG" "$DIR"
 
 sed -i -e "s#${IMAGE}:[0-9]*#$TAG#g" \
---
+++ .devcontainer/workspace/gen-externs.sh
@@ -0,0 +1,24 @@
+set -ex
+
+npx dts2hx \
+    telegraf \
+    twilio \
+    @types/js-cookie \
+    moment \
+    copy-to-clipboard \
+    fastify \
+    @types/accepts \
+    aws-serverless-fastify \
+    google-auth-library \
+    abort-controller \
+    @types/promise-retry \
+    ajv \
+    node-ical \
+    axios \
+    --noLibWrap \
+    --useSystemHaxe \
+    --output lib/dts2hx
+
+sed -i 's/axios.index.AxiosRequestConfig<Dynamic>/axios.AxiosRequestConfig/g' \
+    lib/dts2hx/NodeIcal.hx \
+    lib/dts2hx/node_ical/*.hx
