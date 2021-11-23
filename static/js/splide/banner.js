3cfef3cd85668e4aeffd58c1a241d9682f6f6f04
stella <stella_paradise@yahoo.com.hk>
2021-11-24 01:18:19 +0800

    unignore splide js

---
+++ static/js/splide/banner.js
@@ -0,0 +1,23 @@
+console.log("banner splide before");
+window.onload = function() {
+  new Splide( '.splide', {
+    type: 'loop',
+    autoplay: true,
+    interval: 2000,
+    perPage: 2,
+    perMove: 2,
+    padding: {
+      left: '10%',
+      right: '10%'
+    },
+    breakpoints: {
+      640: {
+        padding: {
+          left: '0%',
+          right: '0%'
+        }
+      },
+    }
+  } ).mount();
+}
+
