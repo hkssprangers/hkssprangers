401ce563303fcf8c6cc2d3cb72d3ad59ec312ccf
Andy Li <andy@onthewings.net>
2021-12-07 12:55:43 +0800

    香辣蟹燴飯

--- src/hkssprangers/info/menu/KCZenzeroMenu.hx
+++ src/hkssprangers/info/menu/KCZenzeroMenu.hx
@@ -16,7 +16,7 @@ enum abstract KCZenzeroItem(String) to String {
     final Single;
 
     static public final all:ReadOnlyArray<KCZenzeroItem> = [
-        // LimitedSpecial,
+        LimitedSpecial,
         HotdogSet,
         NoodleSet,
         PastaSet,
@@ -114,7 +114,7 @@ class KCZenzeroMenu {
         required: ["main"],
     }
 
-    static final limitedSpecial = "鮑汁包子蜆燴飯 $55";
+    static final limitedSpecial = "香辣蟹燴飯 $55";
     static public final KCZenzeroLimitedSpecial = {
         title: "限定：" + limitedSpecial,
         description: "⚠️ 請提早落單。售完即止。",
