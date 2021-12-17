1df3811abf7ffbfb9d618092ed150e02d2031990
stella <stella_paradise@yahoo.com.hk>
2021-12-17 13:12:40 +0800

    change color for Kei Hing

--- src/hkssprangers/server/Index.hx
+++ src/hkssprangers/server/Index.hx
@@ -527,10 +527,10 @@ class Index extends View<IndexProps> {
                             <div className="md:flex"> 
                                 <div className=${thumbnailDivClasses2}>
                                     ${StaticResource.image("/images/keihing.jpg", KeiHing.info().name, "squircle")}
-                                    <p className="absolute align-center-hover text-xs lg:text-lg"><i className="text-blue-500 fas fa-book-open"></i><br/>menu</p>
+                                    <p className="absolute align-center-hover text-xs lg:text-lg"><i className="text-indigo-500 fas fa-book-open"></i><br/>menu</p>
                                 </div>
                                 <div className="md:ml-3 md:pr-6 md:flex-1 flex flex-col justify-center lg:flex-row lg:items-center text-center md:text-left">
-                                    <h1 className=${shopNameClasses}> <i className="md:hidden text-blue-500 fas fa-circle"></i> ${KeiHing.info().name}</h1>
+                                    <h1 className=${shopNameClasses}> <i className="md:hidden text-indigo-500 fas fa-circle"></i> ${KeiHing.info().name}</h1>
                                 </div>
                             </div>
                         </a>
