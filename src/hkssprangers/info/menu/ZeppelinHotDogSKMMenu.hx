d7ee1c9a0b86e4cc4fada5996d61e48894d49221
Andy Li <andy@onthewings.net>
2022-03-12 11:48:50 +0800

    CHOK CHOK粉 地道椒鹽 is out of stock

--- src/hkssprangers/info/menu/ZeppelinHotDogSKMMenu.hx
+++ src/hkssprangers/info/menu/ZeppelinHotDogSKMMenu.hx
@@ -44,7 +44,7 @@ class ZeppelinHotDogSKMMenu {
 
     static public final chokOptions = [
         // "CHOK CHOK粉 芥末紫菜",
-        "CHOK CHOK粉 地道椒鹽",
+        // "CHOK CHOK粉 地道椒鹽",
         "CHOK CHOK粉 惹味麻辣",
         "CHOK CHOK粉 香甜蕃茄",
     ];
