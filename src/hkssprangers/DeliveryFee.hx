1964b15d5e37ed3d2d291211811126285892f816
Andy Li <andy@onthewings.net>
2022-03-25 12:29:33 +0800

    delivery fee

--- src/hkssprangers/DeliveryFee.hx
+++ src/hkssprangers/DeliveryFee.hx
@@ -916,6 +916,19 @@ class DeliveryFee {
             }
         },
         {
+            place: "北河街永富大廈",
+            match: address -> address.contains("永富大廈"),
+            deliveryFee: cluster -> switch cluster {
+                case DragonCentreCluster: 25;
+                case CLPCluster: 25;
+                case GoldenCluster: 25;
+                case SmilingPlazaCluster: 25;
+                case ParkCluster: 25;
+                case PakTinCluster: 35;
+                case TungChauStreetParkCluster: 25;
+            }
+        },
+        {
             place: "翠雲大廈",
             match: address -> address.contains("翠雲大廈"),
             deliveryFee: cluster -> switch cluster {
