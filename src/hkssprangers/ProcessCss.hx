cfa71a90d653578bc5f9945291275042a18e6b4f
Andy Li <andy@onthewings.net>
2022-03-29 15:55:43 +0800

    use "rework" to process css

--- src/hkssprangers/ProcessCss.hx
+++ src/hkssprangers/ProcessCss.hx
@@ -1,66 +1,42 @@
 package hkssprangers;
 
+import js.Node;
 import hkssprangers.StaticResource.*;
 import haxe.io.Path;
 import sys.*;
 import sys.io.*;
 using StringTools;
 
-typedef CssAst = {
-    type:String,
-}
+@:jsRequire("rework")
+extern class Rework {
+    @:selfCall
+    static public function call(css:String, ?options:Dynamic):Rework;
 
-@:jsRequire("css")
-extern class Css {
-    static public function parse(css:String, ?opts:Dynamic):CssAst;
-    static public function stringify(ast:CssAst, ?opts:Dynamic):String;
+    public function use(fn:haxe.Constraints.Function):Rework;
+
+    public function toString(?options:Dynamic):String;
 }
 
 class ProcessCss {
-    static function processCssRules(rules:Array<{type:String}>) {
-        for (rule in rules) {
-            switch (rule.type) {
-                case "rule":
-                    var rule:Dynamic = rule;
-                    processCssDeclarations(rule.declarations);
-                case "media":
-                    var media:Dynamic = rule;
-                    processCssRules(media.rules);
-                case _:
-                    //pass
-            }
-        }
-    }
-    static function processCssDeclarations(declarations:Array<{type:String}>) {
-        var urlValue = ~/^url\((.+)\)$/;
-        for (declaration in declarations)
-        switch (declaration.type) {
-            case "declaration":
-                var declaration:Dynamic = declaration;
-                if (urlValue.match(declaration.value)) {
-                    var url = urlValue.matched(1);
-                    if (!url.startsWith("data:")) {
-                        var staticPath = Path.join([resourcesDir, "css", url]);
-                        var path = staticPath.substr(resourcesDir.length + 1);
-                        if (!FileSystem.exists(staticPath)) {
-                            throw '$url does not exist';
-                        } else {
-                            var h = hash(staticPath);
-                            declaration.value = "url(" + hkssprangers.StaticResource.fingerprint(Path.join(["..", path]), h) + ")";
-                        }
-                    }
-                }
-            case _:
-                // pass
-        }
-    }
     static public function processCss() {
-        var filePath = Path.join([resourcesDir, "css", "style.css"]);
-        var cssString = sys.io.File.getContent(filePath);
-        var css:Dynamic = Css.parse(cssString);
-        // trace(haxe.Json.stringify(css, null, "  "));
-        processCssRules(css.stylesheet.rules);
-        sys.io.File.saveContent(filePath, Css.stringify(css));
+        final filePath = Path.join([resourcesDir, "css", "style.css"]);
+        final cssString = sys.io.File.getContent(filePath);
+
+        final processed = Rework.call(cssString)
+            .use(Node.require("rework-plugin-url")(function(url:String):String {
+                if (url.startsWith("data:"))
+                    return url;
+                final staticPath = Path.join([resourcesDir, "css", url]);
+                final path = staticPath.substr(resourcesDir.length + 1);
+                if (!FileSystem.exists(staticPath)) {
+                    throw '$url does not exist';
+                } else {
+                    final h = hash(staticPath);
+                    return hkssprangers.StaticResource.fingerprint(Path.join(["..", path]), h);
+                }
+            }))
+            .toString();
+        sys.io.File.saveContent(filePath, processed);
     }
 
     static function main():Void {
