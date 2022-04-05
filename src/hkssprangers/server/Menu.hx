4a36a1589dcd37beb6ed2ea724a17135afa18f1c
Andy Li <andy@onthewings.net>
2022-04-05 17:48:11 +0800

    update year's menu
    Years Menus April 2022.pdf

--- src/hkssprangers/server/Menu.hx
+++ src/hkssprangers/server/Menu.hx
@@ -598,6 +598,9 @@ class Menu extends View<MenuProps> {
         var headerClasses = ["p-3", "text-xl", "font-bold"].concat(style.headerClasses).join(" ");
         return jsx('
             <Fragment>
+                <div className="p-3">
+                    ${YearsHKSet.description}
+                </div>
                 <div className=${["border-b-4", "md:flex", "flex-row"].concat(style.borderClasses).join(" ")}>
                     <div className=${["p-3", "md:w-1/2", "md:border-r-4"].concat(style.borderClasses).join(" ")}>
                         <div className=${headerClasses}>${YearsHKSet.properties.main.title}</div>
