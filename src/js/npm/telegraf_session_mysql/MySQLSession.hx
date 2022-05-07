d54b34db6dcbec865ceb8cea8f0f3040ed7f6d7a
Andy Li <andy@onthewings.net>
2022-05-07 14:28:48 +0800

    remove old files

--- src/js/npm/telegraf_session_mysql/MySQLSession.hx
+++
@@ -1,7 +0,0 @@
-package js.npm.telegraf_session_mysql;
-
-@:jsRequire("telegraf-session-mysql")
-extern class MySQLSession {
-    public function new(opts:Dynamic):Void;
-    public function middleware():Dynamic;
-}
