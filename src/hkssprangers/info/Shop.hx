b70a37ed10608346411bc4d4f6f0c226d98d062b
Andy Li <andy@onthewings.net>
2022-05-25 21:48:15 +0800

    KC Zenzero will off

--- src/hkssprangers/info/Shop.hx
+++ src/hkssprangers/info/Shop.hx
@@ -804,7 +804,8 @@ enum abstract Shop(String) to String {
             case [BlackWindow, _, Dinner] if (pickupTimeSlot.start.getTimePart() < "19:00:00"):
                 return Unavailable('晚市最早 19:00 時段交收');
 
-            case [LaksaStore, "2022-05-24", _]:
+            // https://www.facebook.com/permalink.php?story_fbid=535833778234760&id=102717031546439
+            case [KCZenzero, "2022-05-26", _]:
                 return Unavailable('休息一天');
 
             case [CafeGolden, _, Dinner]:
