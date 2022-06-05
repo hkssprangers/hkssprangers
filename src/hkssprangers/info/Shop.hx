2241ac4d7684db31d79dc2804cee529a6db58e2f
Andy Li <andy@onthewings.net>
2022-06-05 22:00:40 +0800

    MGY now closes at 19:00

--- src/hkssprangers/info/Shop.hx
+++ src/hkssprangers/info/Shop.hx
@@ -361,7 +361,7 @@ enum abstract Shop(String) to String {
                     Sunday,
                 ],
                 earliestPickupTime: "12:30:00",
-                latestPickupTime: "20:00:00",
+                latestPickupTime: "18:30:00",
                 isInService: true,
                 facebook: "https://www.facebook.com/MGYVEG",
                 instagram: "https://www.instagram.com/mgyvegan/",
