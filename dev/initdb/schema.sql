52e87a42004b864776d26d42ffe2a5c58d3e9601
Andy Li <andy@onthewings.net>
2022-07-07 10:11:42 +0800

    rerun +db-dump-schema
    looks like new crdb versions no longer output FAMILY lines

--- dev/initdb/schema.sql
+++ dev/initdb/schema.sql
@@ -8,8 +8,7 @@ CREATE TABLE public.courier (
 	"isAdmin" BOOL NOT NULL DEFAULT false,
 	CONSTRAINT courier_pkey PRIMARY KEY ("courierId" ASC),
 	UNIQUE INDEX "courier_UN_tgUsername" ("courierTgUsername" ASC),
-	UNIQUE INDEX "courier_UN_tgId" ("courierTgId" ASC),
-	FAMILY "primary" ("courierId", "courierTgUsername", "courierTgId", "paymeAvailable", "fpsAvailable", deleted, "isAdmin")
+	UNIQUE INDEX "courier_UN_tgId" ("courierTgId" ASC)
 );
 CREATE TABLE public.delivery (
 	"deliveryId" INT8 NOT NULL DEFAULT unique_rowid(),
@@ -32,8 +31,7 @@ CREATE TABLE public.delivery (
 	"customerNote" VARCHAR(2048) NULL,
 	deleted BOOL NOT NULL DEFAULT false,
 	CONSTRAINT delivery_pkey PRIMARY KEY ("deliveryId" ASC),
-	INDEX "delivery_pickupTimeSlotStart" ("pickupTimeSlotStart" ASC),
-	FAMILY "primary" ("deliveryId", "creationTime", "deliveryCode", "pickupLocation", "deliveryFee", "pickupTimeSlotStart", "pickupTimeSlotEnd", "pickupMethod", "paymeAvailable", "fpsAvailable", "customerTgUsername", "customerTgId", "customerTel", "customerWhatsApp", "customerSignal", "customerPreferredContactMethod", "customerBackupContactMethod", "customerNote", deleted)
+	INDEX "delivery_pickupTimeSlotStart" ("pickupTimeSlotStart" ASC)
 );
 CREATE TABLE public."deliveryCourier" (
 	"deliveryId" INT8 NOT NULL,
@@ -42,23 +40,20 @@ CREATE TABLE public."deliveryCourier" (
 	"deliverySubsidy" DECIMAL(12,4) NOT NULL,
 	deleted BOOL NOT NULL DEFAULT false,
 	CONSTRAINT "deliveryCourier_pkey" PRIMARY KEY ("deliveryId" ASC, "courierId" ASC),
-	INDEX "deliveryCourier_FK_courier" ("courierId" ASC),
-	FAMILY "primary" ("deliveryId", "courierId", "deliveryFee", "deliverySubsidy", deleted)
+	INDEX "deliveryCourier_FK_courier" ("courierId" ASC)
 );
 CREATE TABLE public."deliveryOrder" (
 	"deliveryId" INT8 NOT NULL,
 	"orderId" INT8 NOT NULL,
 	deleted BOOL NOT NULL DEFAULT false,
 	CONSTRAINT "deliveryOrder_pkey" PRIMARY KEY ("deliveryId" ASC, "orderId" ASC),
-	INDEX "deliveryOrder_FK_order" ("orderId" ASC),
-	FAMILY "primary" ("deliveryId", "orderId", deleted)
+	INDEX "deliveryOrder_FK_order" ("orderId" ASC)
 );
 CREATE TABLE public."googleFormImport" (
 	"importTime" TIMESTAMPTZ(0) NOT NULL,
 	"spreadsheetId" VARCHAR(100) NOT NULL,
 	"lastRow" INT4 NOT NULL,
-	CONSTRAINT "googleFormImport_pkey" PRIMARY KEY ("importTime" ASC, "spreadsheetId" ASC),
-	FAMILY "primary" ("importTime", "spreadsheetId", "lastRow")
+	CONSTRAINT "googleFormImport_pkey" PRIMARY KEY ("importTime" ASC, "spreadsheetId" ASC)
 );
 CREATE TABLE public."order" (
 	"orderId" INT8 NOT NULL DEFAULT unique_rowid(),
@@ -71,8 +66,7 @@ CREATE TABLE public."order" (
 	"wantTableware" BOOL NOT NULL,
 	"customerNote" VARCHAR(2048) NULL,
 	deleted BOOL NOT NULL DEFAULT false,
-	CONSTRAINT order_pkey PRIMARY KEY ("orderId" ASC),
-	FAMILY "primary" ("orderId", "creationTime", "orderCode", "shopId", "orderDetails", "orderPrice", "platformServiceCharge", "wantTableware", "customerNote", deleted)
+	CONSTRAINT order_pkey PRIMARY KEY ("orderId" ASC)
 );
 CREATE TABLE public.receipt (
 	"receiptId" INT8 NOT NULL DEFAULT unique_rowid(),
@@ -81,8 +75,7 @@ CREATE TABLE public.receipt (
 	"uploaderCourierId" INT8 NULL,
 	CONSTRAINT receipt_pkey PRIMARY KEY ("receiptId" ASC),
 	INDEX "receipt_courier_FK" ("uploaderCourierId" ASC),
-	INDEX "receipt_order_FK" ("orderId" ASC),
-	FAMILY "primary" ("receiptId", "receiptUrl", "orderId", "uploaderCourierId")
+	INDEX "receipt_order_FK" ("orderId" ASC)
 );
 CREATE TABLE public."tgMessage" (
 	"tgMessageId" INT8 NOT NULL DEFAULT unique_rowid(),
@@ -91,8 +84,7 @@ CREATE TABLE public."tgMessage" (
 	"updateType" VARCHAR(50) NULL,
 	"updateData" JSONB NULL,
 	CONSTRAINT "tgMessage_pkey" PRIMARY KEY ("tgMessageId" ASC),
-	INDEX "receiverId" ("receiverId" ASC),
-	FAMILY "primary" ("tgMessageId", "receiverId", "messageData", "updateType", "updateData")
+	INDEX "receiverId" ("receiverId" ASC)
 );
 CREATE TABLE public."tgPrivateChat" (
 	"botId" VARCHAR(128) NOT NULL,
@@ -100,15 +92,13 @@ CREATE TABLE public."tgPrivateChat" (
 	"userUsername" VARCHAR(128) NULL,
 	"chatId" VARCHAR(128) NULL,
 	"lastUpdateData" JSONB NULL,
-	CONSTRAINT "tgPrivateChat_pkey" PRIMARY KEY ("botId" ASC, "userId" ASC),
-	FAMILY "primary" ("botId", "userId", "userUsername", "chatId", "lastUpdateData")
+	CONSTRAINT "tgPrivateChat_pkey" PRIMARY KEY ("botId" ASC, "userId" ASC)
 );
 CREATE TABLE public."twilioMessage" (
 	"twilioMessageId" INT8 NOT NULL DEFAULT unique_rowid(),
 	"creationTime" TIMESTAMPTZ(0) NOT NULL,
 	data JSONB NOT NULL,
-	CONSTRAINT "twilioMessage_pkey" PRIMARY KEY ("twilioMessageId" ASC),
-	FAMILY "primary" ("twilioMessageId", "creationTime", data)
+	CONSTRAINT "twilioMessage_pkey" PRIMARY KEY ("twilioMessageId" ASC)
 );
 CREATE TABLE public._prisma_migrations (
 	id VARCHAR(36) NOT NULL,
@@ -119,8 +109,7 @@ CREATE TABLE public._prisma_migrations (
 	rolled_back_at TIMESTAMPTZ NULL,
 	started_at TIMESTAMPTZ NOT NULL DEFAULT now():::TIMESTAMPTZ,
 	applied_steps_count INT4 NOT NULL DEFAULT 0:::INT8,
-	CONSTRAINT "primary" PRIMARY KEY (id ASC),
-	FAMILY "primary" (id, checksum, finished_at, migration_name, logs, rolled_back_at, started_at, applied_steps_count)
+	CONSTRAINT "primary" PRIMARY KEY (id ASC)
 );
 CREATE TABLE public."menuItem" (
 	"menuItemId" INT8 NOT NULL DEFAULT unique_rowid(),
@@ -131,6 +120,5 @@ CREATE TABLE public."menuItem" (
 	items JSONB NOT NULL,
 	deleted BOOL NOT NULL DEFAULT false,
 	CONSTRAINT "menuItem_pkey" PRIMARY KEY ("menuItemId" ASC),
-	INDEX "menuItem_shop_time" ("shopId" ASC, "startTime" ASC, "endTime" ASC),
-	FAMILY "primary" ("menuItemId", "creationTime", "startTime", "endTime", "shopId", items, deleted)
+	INDEX "menuItem_shop_time" ("shopId" ASC, "startTime" ASC, "endTime" ASC)
 );
