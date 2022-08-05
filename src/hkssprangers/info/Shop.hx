a7205d937d46c427d62f6ae6481a8b13359ff954
Andy Li <andy@onthewings.net>
2022-08-05 10:51:49 +0800

    updating years/the park

--- src/hkssprangers/info/Shop.hx
+++ src/hkssprangers/info/Shop.hx
@@ -808,6 +808,9 @@ enum abstract Shop(String) to String {
             case [BlackWindow, _, Dinner] if (pickupTimeSlot.start.getTimePart() < "19:00:00"):
                 return Unavailable('晚市最早 19:00 時段交收');
 
+            case [YearsHK | TheParkByYears, _, _]:
+                return Unavailable('餐牌更新中');
+
             case [CafeGolden, _, Dinner]:
                 return Unavailable('晚市暫停');
 
