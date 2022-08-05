a63fb74707cf1fdbb72549dfa0f18e96487dd69e
Andy Li <andy@onthewings.net>
2022-08-05 11:12:27 +0800

    Years Menus 3 August 2022.pdf

--- src/hkssprangers/info/Shop.hx
+++ src/hkssprangers/info/Shop.hx
@@ -808,7 +808,7 @@ enum abstract Shop(String) to String {
             case [BlackWindow, _, Dinner] if (pickupTimeSlot.start.getTimePart() < "19:00:00"):
                 return Unavailable('晚市最早 19:00 時段交收');
 
-            case [YearsHK | TheParkByYears, _, _]:
+            case [TheParkByYears, _, _]:
                 return Unavailable('餐牌更新中');
 
             case [CafeGolden, _, Dinner]:
