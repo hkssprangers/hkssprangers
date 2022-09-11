f20178f25331c40e5e39724fff0e2dd3127587d0
Andy Li <andy@onthewings.net>
2022-09-12 00:30:12 +0800

    hana mooncake is over

--- src/hkssprangers/info/menu/HanaSoftCreamMenu.hx
+++ src/hkssprangers/info/menu/HanaSoftCreamMenu.hx
@@ -45,8 +45,8 @@ class HanaSoftCreamMenu {
 
             // https://www.facebook.com/hanasoftcream/posts/pfbid09zG9zEHLdmiJjkS13BioQaJ55YF4yVrpSFT68Xd8jkSL8svuGmmxCG5HS6Mj9EpRl
             // "手工雪糕月餅一盒 (北海道8.0牛乳×2 意大利黑朱古力×2) $278",
-            "手工雪糕月餅一個(北海道8.0牛乳) $78",
-            "手工雪糕月餅一個(意大利黑朱古力) $78",
+            // "手工雪糕月餅一個(北海道8.0牛乳) $78",
+            // "手工雪糕月餅一個(意大利黑朱古力) $78",
         ]
     };
 
