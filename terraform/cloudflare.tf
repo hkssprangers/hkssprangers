781ae654fc28ca2f24477bacbdbef9b42498713b
Andy Li <andy@onthewings.net>
2022-09-15 11:48:42 +0800

    remove the no-in-use aws acm cert

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -2,13 +2,6 @@ resource "cloudflare_zone" "ssprangers" {
   zone = "ssprangers.com"
 }
 
-resource "cloudflare_record" "aws-acm" {
-  zone_id = cloudflare_zone.ssprangers.id
-  name    = tolist(aws_acm_certificate.default.domain_validation_options).0.resource_record_name
-  value   = trimsuffix(tolist(aws_acm_certificate.default.domain_validation_options).0.resource_record_value, ".")
-  type    = tolist(aws_acm_certificate.default.domain_validation_options).0.resource_record_type
-}
-
 resource "cloudflare_record" "ssprangers" {
   zone_id = cloudflare_zone.ssprangers.id
   name    = "ssprangers.com"
