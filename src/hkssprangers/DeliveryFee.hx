8a9929f71b587f64901a6e0f8710417e051b33ee
Andy Li <andy@onthewings.net>
2023-06-28 17:56:26 +0100

    城市的盼望 delivery fee

--- src/hkssprangers/DeliveryFee.hx
+++ src/hkssprangers/DeliveryFee.hx
@@ -3310,6 +3310,25 @@ class DeliveryFee {
             }
         },
         {
+            place: "城市的盼望", //南昌街156-158
+            osm: [
+                {
+                    url: "https://www.openstreetmap.org/node/11012205752"
+                }
+            ],
+            match: address -> address.contains("城市的盼望"),
+            deliveryFee: cluster -> switch cluster {
+                case DragonCentreCluster: 25;
+                case PeiHoStreetMarketCluster: 25;
+                case CLPCluster: 25;
+                case GoldenCluster: 25;
+                case SmilingPlazaCluster: 25;
+                case ParkCluster: 25;
+                case PakTinCluster: 25;
+                case TungChauStreetParkCluster: 25;
+            }
+        },
+        {
             place: "長明大廈 (近楓樹街遊樂場)",
             osm: [
                 {
